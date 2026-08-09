fdbc9557c747e5875023d3ae2ed916dad7c46086
unknown <nayemk295@gmail.com>
2026-08-09 20:31:24 +0600

    fix Cloudflare deployment configuration

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_EMAILJS_TEMPLATE_ID":"template_olb1f3b","NEXT_PUBLIC_EMAILJS_SERVICE_ID":"service_bgu8lxk","NEXT_PUBLIC_EMAILJS_PUBLIC_KEY":"HZ6MQjAZydC3zxstA"};
+export const development = {"NEXT_PUBLIC_EMAILJS_TEMPLATE_ID":"template_olb1f3b","NEXT_PUBLIC_EMAILJS_SERVICE_ID":"service_bgu8lxk","NEXT_PUBLIC_EMAILJS_PUBLIC_KEY":"HZ6MQjAZydC3zxstA"};
+export const test = {"NEXT_PUBLIC_EMAILJS_TEMPLATE_ID":"template_olb1f3b","NEXT_PUBLIC_EMAILJS_SERVICE_ID":"service_bgu8lxk","NEXT_PUBLIC_EMAILJS_PUBLIC_KEY":"HZ6MQjAZydC3zxstA"};
